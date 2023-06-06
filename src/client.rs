4486c759eec5da92a17202f0efbd3f423b018273
Cláuvin <Clauvin@users.noreply.github.com>
2023-06-06 20:33:25 -0300

    [basic_clock] We have a proper drawn white square

--- src/client.rs
+++ src/client.rs
@@ -2,18 +2,15 @@ use ambient_api::prelude::*;
 
 fn CreateWhiteBackground() -> Element {
     Rectangle.el()
-    .with(width(), 2000.)
-    .with(height(), 2000.)
-    .with(background_color(), vec4(0., 0., 0., 1.))
+    .with(width(), 500.)
+    .with(height(), 500.)
+    .with(background_color(), vec4(1., 1., 1., 1.))
 }
 
 #[element_component]
 fn App(_hooks: &mut Hooks) -> Element {
     Group::el([
-        FlowColumn::el([
-            Rectangle.el(),
-            CreateWhiteBackground(),
-        ]),
+        CreateWhiteBackground(),
         Line.el()
             .with(line_from(), vec3(200., 200., 0.))
             .with(line_to(), vec3(300., 200., 0.))
