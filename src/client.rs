c39be7126d93a7cff395e313ee1391390d61fb85
Cláuvin <Clauvin@users.noreply.github.com>
2023-06-06 21:07:14 -0300

    [basic_clock] We have a point maker now

--- src/client.rs
+++ src/client.rs
@@ -10,8 +10,8 @@ fn CreateWhiteBackground() -> Element {
 fn DrawPoint(x: f32, y: f32, depth: f32) -> Element {
     Line.el()
         .with(line_from(), vec3(x, y, depth))
-        .with(line_to(), vec3(x+0.001, y+0.001, depth))
-        .with(line_width(), 10.)
+        .with(line_to(), vec3(x+1., y+1., depth))
+        .with(line_width(), 1.)
         .with(background_color(), vec4(0., 1., 0., 1.))
 }
 
@@ -19,7 +19,7 @@ fn DrawPoint(x: f32, y: f32, depth: f32) -> Element {
 fn App(_hooks: &mut Hooks) -> Element {
     Group::el([
         
-        DrawPoint(250., 250., -1.),
+        DrawPoint(250., 250., 0.),
         //CreateWhiteBackground(),
     ])
 }
