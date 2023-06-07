5b81a926055117a7af2b3de25f39e85a4d4cecd5
Cláuvin <Clauvin@users.noreply.github.com>
2023-06-06 21:00:35 -0300

    [basic_clock] I can draw a line, but not a point

--- src/client.rs
+++ src/client.rs
@@ -2,30 +2,25 @@ use ambient_api::prelude::*;
 
 fn CreateWhiteBackground() -> Element {
     Rectangle.el()
-    .with(width(), 500.)
-    .with(height(), 500.)
-    .with(background_color(), vec4(1., 1., 1., 1.))
+        .with(width(), 500.)
+        .with(height(), 500.)
+        .with(background_color(), vec4(1., 1., 1., 1.))
+}
+
+fn DrawPoint(x: f32, y: f32, depth: f32) -> Element {
+    Line.el()
+        .with(line_from(), vec3(x, y, depth))
+        .with(line_to(), vec3(x+0.001, y+0.001, depth))
+        .with(line_width(), 10.)
+        .with(background_color(), vec4(0., 1., 0., 1.))
 }
 
 #[element_component]
 fn App(_hooks: &mut Hooks) -> Element {
     Group::el([
-        CreateWhiteBackground(),
-        Line.el()
-            .with(line_from(), vec3(200., 200., 0.))
-            .with(line_to(), vec3(300., 200., 0.))
-            .with(line_width(), 1.)
-            .with(background_color(), vec4(1., 0., 0., 1.)),
-        Line.el()
-            .with(line_from(), vec3(200., 200., 0.))
-            .with(line_to(), vec3(200., 300., 0.))
-            .with(line_width(), 1.)
-            .with(background_color(), vec4(0., 1., 0., 1.)),
-        Line.el()
-            .with(line_from(), vec3(200., 200., 0.))
-            .with(line_to(), vec3(500., 300., 0.))
-            .with(line_width(), 10.)
-            .with(background_color(), vec4(0., 0., 1., 1.)),
+        
+        DrawPoint(250., 250., -1.),
+        //CreateWhiteBackground(),
     ])
 }
 
