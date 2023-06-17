dc733103869121279c5d9d35fc353ec3b65a1d9a
Cláuvin <Clauvin@users.noreply.github.com>
2023-06-17 13:37:52 -0300

    [clock_reactiveness] Clock reacts to the size of the window, always expanding to how squarely big it can be

--- src/client.rs
+++ src/client.rs
@@ -3,17 +3,6 @@ use std::f32::consts::PI;
 use core::time::Duration;
 use chrono::prelude::*;
 
-
-const CLOCK_RAY: f32 = 250.;
-const CLOCK_X_POSITION: f32 = 375.;
-const CLOCK_Y_POSITION: f32 = 300.;
-const CLOCK_X_CENTER: f32 = CLOCK_X_POSITION;
-const CLOCK_Y_CENTER: f32 = CLOCK_Y_POSITION;
-
-const HOUR_RAY: f32 = CLOCK_RAY/4.;
-const MINUTE_RAY: f32 = CLOCK_RAY/3.;
-const SECOND_RAY: f32 = CLOCK_RAY/2.;
-
 const CLOCK_BORDER_COLOR: Vec4 = vec4(0.2, 0.1, 0.6, 1.);
 const HOUR_COLOR: Vec4 = CLOCK_BORDER_COLOR;
 const MINUTE_COLOR: Vec4 = vec4(0.2, 0.6, 0.1, 1.);
@@ -69,6 +58,7 @@ fn get_current_seconds(date_and_time: DateTime<Local>) -> u32 {
 
 #[element_component]
 fn App(_hooks: &mut Hooks) -> Element {
+
     let size_info = _hooks.use_query(window_logical_size());
 
     let (now, set_now) = _hooks.use_state(time());
@@ -78,27 +68,52 @@ fn App(_hooks: &mut Hooks) -> Element {
     let initial_clock_minute = get_current_minutes(initial_date_and_time) as f32;
     let initial_clock_second = get_current_seconds(initial_date_and_time) as f32;
 
-    let (hour_x, set_hour_x) = _hooks.use_state(CLOCK_X_CENTER);
-    let (hour_y, set_hour_y) = _hooks.use_state(CLOCK_Y_CENTER - HOUR_RAY);
-    let (minute_x, set_minute_x) = _hooks.use_state(CLOCK_X_CENTER);
-    let (minute_y, set_minute_y) = _hooks.use_state(CLOCK_Y_CENTER - MINUTE_RAY);
-    let (second_x, set_second_x) = _hooks.use_state(CLOCK_X_CENTER);
-    let (second_y, set_second_y) = _hooks.use_state(CLOCK_Y_CENTER - SECOND_RAY);
+    let (clock_ray, set_clock_ray) = _hooks.use_state(300.);
+    let (clock_x_position, set_clock_x_position) = _hooks.use_state(clock_ray);
+    let (clock_y_position, set_clock_y_position) = _hooks.use_state(clock_ray);
+    let (clock_x_center, set_clock_x_center) = _hooks.use_state(clock_x_position);
+    let (clock_y_center, set_clock_y_center) = _hooks.use_state(clock_y_position);
+
+    let hour_ray: f32 = clock_ray/4.;
+    let minute_ray: f32 = clock_ray/3.;
+    let second_ray: f32 = clock_ray/2.;
+
+    let (hour_x, set_hour_x) = _hooks.use_state(clock_x_center);
+    let (hour_y, set_hour_y) = _hooks.use_state(clock_y_center - hour_ray);
+    let (minute_x, set_minute_x) = _hooks.use_state(clock_x_center);
+    let (minute_y, set_minute_y) = _hooks.use_state(clock_y_center - minute_ray);
+    let (second_x, set_second_x) = _hooks.use_state(clock_x_center);
+    let (second_y, set_second_y) = _hooks.use_state(clock_y_center - second_ray);
 
     let (hour_phase, set_hour_phase) = _hooks.use_state(initial_clock_hour * PI/12.);
     let (minute_phase, set_minute_phase) = _hooks.use_state(initial_clock_minute * PI/30.);
     let (second_phase, set_second_phase) = _hooks.use_state(initial_clock_second * PI/30.);
 
-    set_hour_x(CLOCK_X_CENTER + HOUR_RAY*(hour_phase.sin())+0.1);
-    set_hour_y(CLOCK_Y_CENTER - HOUR_RAY*(hour_phase.cos())-0.1);
+    set_hour_x(clock_x_center + hour_ray*(hour_phase.sin())+0.1);
+    set_hour_y(clock_y_center - hour_ray*(hour_phase.cos())-0.1);
 
-    set_minute_x(CLOCK_X_CENTER + MINUTE_RAY*(minute_phase.sin())+0.1);
-    set_minute_y(CLOCK_Y_CENTER - MINUTE_RAY*(minute_phase.cos())-0.1);
+    set_minute_x(clock_x_center + minute_ray*(minute_phase.sin())+0.1);
+    set_minute_y(clock_y_center - minute_ray*(minute_phase.cos())-0.1);
 
-    set_second_x(CLOCK_X_CENTER + SECOND_RAY*(second_phase.sin())+0.1);
-    set_second_y(CLOCK_Y_CENTER - SECOND_RAY*(second_phase.cos())-0.1);
+    set_second_x(clock_x_center + second_ray*(second_phase.sin())+0.1);
+    set_second_y(clock_y_center - second_ray*(second_phase.cos())-0.1);
 
     _hooks.use_frame(move |world|{
+        let window_width = size_info[0].1.x;
+        let window_height = size_info[0].1.y;
+
+        let mut center = 0.;
+
+        if window_width <= window_height{
+            center = (window_width/2) as f32;
+        } else {
+            center = (window_height/2) as f32;
+        }
+
+        set_clock_ray(center);
+        set_clock_x_center(center); set_clock_y_center(center);
+        set_clock_x_position(center); set_clock_y_position(center);
+
         let latest = time();
         if latest - now > Duration::from_secs_f32(1.0).as_secs_f32() {
             set_now(latest);
@@ -127,22 +142,22 @@ fn App(_hooks: &mut Hooks) -> Element {
             // for some reason, second 45 without 0.1 won't show.
             // Maybe it happens with minute and hour 45 too, so I'm adding the same fix to those as well
             // That said, I need to check those later, properly
-            set_hour_x(CLOCK_X_CENTER + HOUR_RAY*(hour_phase.sin())+0.1);
-            set_hour_y(CLOCK_Y_CENTER - HOUR_RAY*(hour_phase.cos())-0.1);
+            set_hour_x(clock_x_center + hour_ray*(hour_phase.sin())+0.1);
+            set_hour_y(clock_y_center - hour_ray*(hour_phase.cos())-0.1);
 
-            set_minute_x(CLOCK_X_CENTER + MINUTE_RAY*(minute_phase.sin())+0.1);
-            set_minute_y(CLOCK_Y_CENTER - MINUTE_RAY*(minute_phase.cos())-0.1);
+            set_minute_x(clock_x_center + minute_ray*(minute_phase.sin())+0.1);
+            set_minute_y(clock_y_center - minute_ray*(minute_phase.cos())-0.1);
 
-            set_second_x(CLOCK_X_CENTER + SECOND_RAY*(second_phase.sin())+0.1);
-            set_second_y(CLOCK_Y_CENTER - SECOND_RAY*(second_phase.cos())-0.1);
+            set_second_x(clock_x_center + second_ray*(second_phase.sin())+0.1);
+            set_second_y(clock_y_center - second_ray*(second_phase.cos())-0.1);
         }
     });
 
     Group::el([
-        draw_circle(CLOCK_X_POSITION, CLOCK_Y_POSITION, CLOCK_RAY, CLOCK_BORDER_COLOR),
-        draw_static_hour_hand(CLOCK_X_CENTER, CLOCK_Y_CENTER, hour_x, hour_y),
-        draw_static_minute_hand(CLOCK_X_CENTER, CLOCK_Y_CENTER, minute_x, minute_y),
-        draw_static_second_hand(CLOCK_X_CENTER, CLOCK_Y_CENTER, second_x, second_y),
+        draw_circle(clock_x_position, clock_y_position, clock_ray, CLOCK_BORDER_COLOR),
+        draw_static_hour_hand(clock_x_center, clock_y_center, hour_x, hour_y),
+        draw_static_minute_hand(clock_x_center, clock_y_center, minute_x, minute_y),
+        draw_static_second_hand(clock_x_center, clock_y_center, second_x, second_y),
     ])
 }
 
