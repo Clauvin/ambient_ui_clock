5f7b03899222a17c1bd150dfd449f8e0a8b62018
Cláuvin <Clauvin@users.noreply.github.com>
2023-06-22 16:55:19 -0300

    [clock_reactiveness] Passed test code to tests library

--- src/client.rs
+++ src/client.rs
@@ -5,6 +5,7 @@ use chrono::prelude::*;
 
 mod drawing;
 mod clock_time;
+mod tests;
 
 #[element_component]
 fn App(_hooks: &mut Hooks) -> Element {
@@ -121,7 +122,7 @@ fn App(_hooks: &mut Hooks) -> Element {
 #[main]
 pub fn main() {
     println!("{:?}", Local::now());
-    color_tests();
+    tests::color_tests();
 	start();
 }
 
@@ -129,40 +130,3 @@ fn start(){
 	App.el().spawn_interactive();
 }
 
-//#[test]
-fn color_test(color: Vec4) {
-	let color_lower_limit = 0.;
-	let color_higher_limit = 1.;
-	more_asserts::debug_assert_ge!(color.x, color_lower_limit, "X value of color is smaller than {}", color_lower_limit);
-	more_asserts::debug_assert_ge!(color.y, color_lower_limit, "Y value of color is smaller than {}", color_lower_limit);
-	more_asserts::debug_assert_ge!(color.z, color_lower_limit, "Z value of color is smaller than {}", color_lower_limit);
-	more_asserts::debug_assert_ge!(color.w, color_lower_limit, "W value of color is smaller than {}", color_lower_limit);
-	more_asserts::debug_assert_le!(color.x, color_higher_limit, "X value of color is bigger than {}", color_higher_limit);
-	more_asserts::debug_assert_le!(color.y, color_higher_limit, "Y value of color is bigger than {}", color_higher_limit);
-	more_asserts::debug_assert_le!(color.z, color_higher_limit, "Z value of color is bigger than {}", color_higher_limit);
-	more_asserts::debug_assert_le!(color.w, color_higher_limit, "W value of color is bigger than {}", color_higher_limit);
-}
-
-fn clock_border_color_test(){
-	color_test(drawing::CLOCK_BORDER_COLOR);
-}
-
-fn hour_color_test(){
-	color_test(drawing::HOUR_COLOR);
-}
-
-fn minute_color_test(){
-	color_test(drawing::MINUTE_COLOR);
-}
-
-fn second_color_test(){
-	color_test(drawing::SECOND_COLOR);
-}
-
-//the etc_color variables are Vec4 used for the clock colors
-fn color_tests(){
-	clock_border_color_test();
-	hour_color_test();
-	minute_color_test();
-	second_color_test();
-}
