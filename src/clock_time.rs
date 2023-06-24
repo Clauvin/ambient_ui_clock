120f3ce5e9c649620cfa2f8483e9e72c612c3ee0
Cláuvin <Clauvin@users.noreply.github.com>
2023-06-24 15:15:32 -0300

    [clock_reactiveness] Added get_initial_hour/minute/second_phase functions to clock_time

--- src/clock_time.rs
+++ src/clock_time.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use std::f32::consts::PI;
 
 pub fn get_current_date_and_time() -> DateTime<Local>{
     Local::now()
@@ -20,4 +21,16 @@ pub fn get_current_seconds(date_and_time: DateTime<Local>) -> u32 {
 //Once it is, I will update the code to properly consider GMT.
 pub fn get_current_time_zone() -> i32{
     Local::now().offset().local_minus_utc()
+}
+
+pub fn get_initial_hour_phase(initial_clock_hour: f32, initial_clock_minute: f32, initial_clock_second: f32) -> f32 {
+    initial_clock_hour * PI/6. + initial_clock_minute * PI/6. * 1./60. + initial_clock_second * PI/6. * 1./60. * 1./60.
+}
+
+pub fn get_initial_minute_phase(initial_clock_minute: f32, initial_clock_second: f32) -> f32 {
+    initial_clock_minute * PI/30. + initial_clock_second * PI/30. * 1./60.
+}
+
+pub fn get_initial_second_phase(initial_clock_second: f32) -> f32 {
+    initial_clock_second * PI/30.
 }
